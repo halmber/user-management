16426b44a4acc772656f08997310047ae0c6cae4
halmber <rogozyanskiy.bogdan@gmail.com>
2024-09-02 19:00:02 +0300

    style: change table header color

--- src/components/TableHeader.tsx
+++ src/components/TableHeader.tsx
@@ -7,13 +7,13 @@ interface TableHeaderProps {
 
 const TableHeader: FC<TableHeaderProps> = ({ columns }) => {
   return (
-    <thead className="bg-gray-50">
+    <thead className="bg-gray-300">
       <tr>
         {columns.map((header) => (
           <th
             key={header}
             scope="col"
-            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+            className="px-6 py-3 text-left text-xs font-medium text-black uppercase tracking-wider"
           >
             {header}
           </th>
